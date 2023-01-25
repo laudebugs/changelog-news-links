5560393234d4ed04918d7a3196519f8e33675715
laudebugs <lbugasu@gmail.com>
2023-01-24 21:44:19 -0500

    feat: filter out unlikely links

--- src/update-links.ts
+++ src/update-links.ts
@@ -1,45 +1,44 @@
 import { launch } from 'puppeteer'
 import { writeFileSync, readFileSync } from 'fs'
 import { Article } from './types.js'
-import { filterLinks, filterWorkingLinks } from './utils.js'
-const BASE_URL = "https://changelog.com/"
+import { filterLinks } from './utils.js'
+const BASE_URL = 'https://changelog.com/'
 
 const browser = await launch()
 const webPage = await browser.newPage()
 
-
-const oldArticles: Array<Article> =JSON.parse( readFileSync('./data/links.json', 'utf-8')) as Array<Article>
+const oldArticles: Array<Article> = JSON.parse(readFileSync('./data/links.json', 'utf-8')) as Array<Article>
 const newArticles = new Array<Article>()
 
-for(let page = 0; page<=345; page++){
+for (let page = 0; page <= 345; page++) {
     let url = `${BASE_URL}?page=${page}#feed`
     try {
         await webPage.goto(url, {
             waitUntil: 'networkidle0',
-            timeout: 60000
+            timeout: 60000,
         })
         const feedArticles = await webPage.$$('article')
         let articleLogged = false
-        for await(const articleElement of feedArticles){
+        for await (const articleElement of feedArticles) {
             const articleTitleElement = await articleElement.$('.news_item-title')
             const linkElement = await articleTitleElement?.$('a')
-            if(!articleTitleElement || !linkElement) continue
+            if (!articleTitleElement || !linkElement) continue
 
             /* Get the Article link and Title */
-            const link = await webPage.evaluate(linkElement => linkElement.getAttribute('href'), linkElement)
-            const title = await webPage.evaluate(h2Element => h2Element.textContent, articleTitleElement)
+            const link = await webPage.evaluate((linkElement) => linkElement.getAttribute('href'), linkElement)
+            const title = await webPage.evaluate((h2Element) => h2Element.textContent, articleTitleElement)
 
             const dateEle = await articleElement.$('span[data-style="date"]')
-            const date = await webPage.evaluate(dateElement => dateElement?.textContent, dateEle)
-            const article = {title, link, date}
-            articleLogged = !!oldArticles.find((article)=>article.link === link)
-            if(articleLogged) {
+            const date = await webPage.evaluate((dateElement) => dateElement?.textContent, dateEle)
+            const article = { title, link, date }
+            articleLogged = !!oldArticles.find((article) => article.link === link)
+            if (articleLogged) {
                 console.log('article logged')
                 break
             }
             newArticles.push(article)
         }
-        if(articleLogged) break
+        if (articleLogged) break
     } catch (error: any) {
         console.log(`error encountered: ${error?.message}`)
     }
@@ -48,6 +47,7 @@ for(let page = 0; page<=345; page++){
 await browser.close()
 
 writeFileSync('./data/links.json', JSON.stringify([...newArticles, ...oldArticles], null, 4))
+
 const filteredLinks = filterLinks([...newArticles, ...oldArticles])
 writeFileSync('./data/filtered-links.json', JSON.stringify(filteredLinks, null, 4))
 // const workingLinks = await filterWorkingLinks(filteredLinks)
--- src/utils.ts
+++ src/utils.ts
@@ -1,17 +1,23 @@
-import { Article } from "./types.js";
+import { Article } from './types.js'
 
 const BlacklistedDomains = ['changelog.com', 'github.com']
-
-export const filterLinks = (articles: Array<Article>) =>{
-    return articles.filter(({link}) => !BlacklistedDomains.some(domain => link?.includes(domain)))
+const pathRegex = /^(https?:\/\/[^\/]+)(?:\/)([^?]*)/
+export const filterLinks = (articles: Array<Article>) => {
+    return articles.filter(({ link }) => {
+        if (!link) return false
+        let match = link.match(pathRegex)
+        if (link[link.length - 1] == '/') match = link.substring(0, link.length - 1).match(pathRegex)
+        const isLikelyBlogPost = match && match.length >= 2
+        return !BlacklistedDomains.some((domain) => link?.includes(domain) || !isLikelyBlogPost)
+    })
 }
 
-export const filterWorkingLinks = async (articles: Array<Article>)=>{
+export const filterWorkingLinks = async (articles: Array<Article>) => {
     const workingLinks = new Array<Article>()
-    for await (const article of articles){
-        if(!article.link) continue
+    for await (const article of articles) {
+        if (!article.link) continue
         const resonse = await fetch(article.link)
-        if (resonse.ok){
+        if (resonse.ok) {
             workingLinks.push(article)
         }
     }
