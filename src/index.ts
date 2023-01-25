366fac71686deebddccea1872fc775270144011f
laudebugs <lbugasu@gmail.com>
2023-01-24 20:33:46 -0500

    feat: initialize link generation

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import { launch } from 'puppeteer'
+import { writeFileSync, readFileSync } from 'fs'
+const BASE_URL = "https://changelog.com/"
+
+const browser = await launch()
+const webPage = await browser.newPage()
+
+type Article = {title: string, link: string, date: string}
+type Nullable<T> = {[K in keyof T]: T[K] | null | undefined}
+
+const oldArticles: Array<Nullable<Article>> = readFileSync('./data/links.json') as unknown as Array<Nullable<Article>>
+const newArticles = new Array<Nullable<Article>>()
+
+for(let page = 0; page<=345; page++){
+    let url = `${BASE_URL}?page=${page}#feed`
+    try {
+        await webPage.goto(url, {
+            waitUntil: 'networkidle0',
+            timeout: 60000
+        })
+        const feedArticles = await webPage.$$('article')
+        
+        for await( const articleElement of feedArticles){
+            const articleTitleElement = await articleElement.$('.news_item-title')
+            const linkElement = await articleTitleElement?.$('a')
+            if(!articleTitleElement || !linkElement) continue
+
+            /* Get the Article link and Title */
+            const link = await webPage.evaluate(linkElement => linkElement.getAttribute('href'), linkElement)
+            const title = await webPage.evaluate(h2Element => h2Element.textContent, articleTitleElement)
+
+            const dateEle = await articleElement.$('span[data-style="date"]')
+            const date = await webPage.evaluate(dateElement => dateElement?.textContent, dateEle)
+            const article = {title, link, date}
+            const articleLogged = oldArticles.find((article)=>article.link === link)
+            if(articleLogged) break
+            newArticles.push(article)
+        }
+    } catch (error: any) {
+        console.log(`error encountered: ${error?.message}`)
+    }
+}
+
+await browser.close()
+
+writeFileSync('./data/links.json', JSON.stringify([...newArticles, ...oldArticles], null, 4))
---
+++ src/init-link-generation.ts
@@ -0,0 +1,46 @@
+import { launch } from 'puppeteer'
+import { writeFileSync } from 'fs'
+const BASE_URL = "https://changelog.com/"
+
+const browser = await launch()
+const webPage = await browser.newPage()
+
+type Article = {title: string, link: string, date: string}
+type Nullable<T> = {[K in keyof T]: T[K] | null | undefined}
+
+const articles: Array<Nullable<Article>> = []
+
+for(let page = 0; page<=345; page++){
+    let url = `${BASE_URL}?page=${page}#feed`
+    try {
+        await webPage.goto(url, {
+            waitUntil: 'networkidle0',
+            timeout: 60000
+        })
+        const feedArticles = await webPage.$$('article')
+        
+        for await( const articleElement of feedArticles){
+            const articleTitleElement = await articleElement.$('.news_item-title')
+            const linkElement = await articleTitleElement?.$('a')
+            if(!articleTitleElement || !linkElement) continue
+
+            /* Get the Article link and Title */
+            const link = await webPage.evaluate(linkElement => linkElement.getAttribute('href'), linkElement)
+            const title = await webPage.evaluate(h2Element => h2Element.textContent, articleTitleElement)
+
+            const dateEle = await articleElement.$('span[data-style="date"]')
+            const date = await webPage.evaluate(dateElement => dateElement?.textContent, dateEle)
+            const article = {title, link, date}
+            const articleLogged = articles.find((article)=>article.link === link)
+            if(articleLogged) break
+            articles.push(article)
+        }
+        console.log(`done ${page} of 345`)
+    } catch (error: any) {
+        console.log(`error encountered: ${error?.message}`)
+    }
+}
+
+await browser.close()
+
+writeFileSync('./data/links.json', JSON.stringify(articles, null, 4))
